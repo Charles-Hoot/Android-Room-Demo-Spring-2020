28a13353093c74ab733fc5c97a8081fa99f9115b
Charles Hoot <hoot@nwmissouri.edu>
2020-03-30 22:22:33 -0500

    Button method accesses database

--- app/src/main/java/com/example/roomdemo/MainActivity.java
+++ app/src/main/java/com/example/roomdemo/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.roomdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AnimalsDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = Room.databaseBuilder(getApplicationContext(),
+                AnimalsDatabase.class,
+                "animals_db").build();
+
 
         Button addBTN = findViewById(R.id.addBTN);
         addBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("Button", "add button clicked");
+                EditText inputET = findViewById(R.id.inputET);
+                String theName = inputET.getText().toString();
+                Cat myCat = new Cat();
+                myCat.name = theName;
+                myCat.weight = 10.0;
+                db.catDao().insert(myCat);
+                List<Cat> catList = db.catDao().getAll();
+                for(Cat cat:catList){
+                    Log.d("DB", "cat: " +cat.id
+                            + " named " + cat.name
+                            + " weighs" + cat.weight);
+                }
+
             }
         });
 
